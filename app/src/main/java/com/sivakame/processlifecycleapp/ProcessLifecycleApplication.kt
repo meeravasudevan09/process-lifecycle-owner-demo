f4a968b0f7bc7e1a251a8f4966b81414697a54fd
Meera Vasudevan <meera.vasudevan@rea-group.com>
2021-04-30 16:33:05 +1000

    Add toasts to indicate background/foreground

--- app/src/main/java/com/sivakame/processlifecycleapp/ProcessLifecycleApplication.kt
+++ app/src/main/java/com/sivakame/processlifecycleapp/ProcessLifecycleApplication.kt
@@ -2,13 +2,17 @@ package com.sivakame.processlifecycleapp
 
 import android.app.Application
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ProcessLifecycleOwner
 
 
-class ProcessLifecycleApplication: Application(), LifecycleObserver {
+class ProcessLifecycleApplication : Application(), LifecycleObserver {
+    companion object {
+        private const val TAG = "ProcessLifecycleApp"
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -17,11 +21,13 @@ class ProcessLifecycleApplication: Application(), LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onAppBackgrounded() {
-        Log.d("MEERA", "App in background")
+        Toast.makeText(this, "Goodbye!", Toast.LENGTH_LONG).show()
+        Log.d(TAG, "App in background")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onAppForegrounded() {
-        Log.d("MEERA", "App in foreground")
+        Toast.makeText(this, "Welcome to the ProcessLifecycleApp", Toast.LENGTH_LONG).show()
+        Log.d(TAG, "App in foreground")
     }
 }
